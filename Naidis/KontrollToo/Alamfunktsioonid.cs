510f775700ca2839e765b05e622f6be61149a8c5
opilane <opilane@HP-CZC2349HTZ>
2026-02-26 13:38:47 +0200

    KT

---
+++ Naidis/KontrollToo/Alamfunktsioonid.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Alamfunktsioonid
+{
+  
+    public static void KytuseKalkulaator()
+    {
+        Console.Write("Sisesta läbitud vahemaa (km): ");
+        double km = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Sisesta kütusekulu 100km kohta (L): ");
+        double kulu100 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Sisesta kütuse hind (€): ");
+        double hind = Convert.ToDouble(Console.ReadLine());
+
+        double kulunudKytus = (km / 100) * kulu100;
+        double maksumus = kulunudKytus * hind;
+
+        Console.WriteLine($"\nKulunud kütus: {kulunudKytus:F2} L");
+        Console.WriteLine($"Reisi maksumus: {maksumus:F2} €\n");
+    }
+
+  
+    public static string HindaIsikukood(string isikukood)
+    {
+        if (isikukood.Length != 11)
+            return "Viga: Isikukood peab olema 11-kohaline!";
+
+        char esimene = isikukood[0];
+
+        string sugu;
+        int sajand;
+
+        if (esimene == '1' || esimene == '3' || esimene == '5')
+            sugu = "Mees";
+        else if (esimene == '2' || esimene == '4' || esimene == '6')
+            sugu = "Naine";
+        else
+            sugu = "Tundmatu";
+
+        if (esimene == '1' || esimene == '2') sajand = 1800;
+        else if (esimene == '3' || esimene == '4') sajand = 1900;
+        else if (esimene == '5' || esimene == '6') sajand = 2000;
+        else sajand = 0;
+
+        string aastaStr = isikukood.Substring(1, 2);
+        string kuu = isikukood.Substring(3, 2);
+        string paev = isikukood.Substring(5, 2);
+
+        int aasta;
+        if (!int.TryParse(aastaStr, out aasta))
+            return "Viga: Vigane sünniaasta!";
+
+        aasta += sajand;
+
+        return $"Oled {sugu}, sündinud {paev}.{kuu}.{aasta}";
+    }
+
+   
+    public static void TaringuMang()
+    {
+        Random rnd = new Random();
+        List<int> summad = new List<int>();
+        int duublid = 0;
+        int kogusumma = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            int t1 = rnd.Next(1, 7);
+            int t2 = rnd.Next(1, 7);
+
+            if (t1 == t2)
+                duublid++;
+
+            int summa = t1 + t2;
+            summad.Add(summa);
+            kogusumma += summa;
+        }
+
+        Console.WriteLine("\nVisked:");
+        foreach (int s in summad)
+            Console.Write(s + " ");
+
+        Console.WriteLine($"\nDuubleid visati: {duublid} korda");
+        Console.WriteLine($"Kõikide visete kogusumma: {kogusumma}\n");
+    }
+
+    public static Tuple<double, double> ArvutaPalk(double brutopalk)
+    {
+        double maksuvaba = 0;
+
+        if (brutopalk < 1200)
+            maksuvaba = 654;
+
+        double tootuskindlustus = brutopalk * 0.016;
+        double kogumispension = brutopalk * 0.02;
+
+        double tulumaksuAlus = brutopalk - maksuvaba;
+        if (tulumaksuAlus < 0)
+            tulumaksuAlus = 0;
+
+        double tulumaks = tulumaksuAlus * 0.20;
+
+        double neto = brutopalk - tulumaks - tootuskindlustus - kogumispension;
+
+        return Tuple.Create(maksuvaba, neto);
+    }
+}
---
+++ Naidis/KontrollToo/StartPage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class StartPage
+{
+    static void Main(string[] args)
+    {
+        bool tootab = true;
+
+        while (tootab)
+        {
+            Console.WriteLine("Vali tegevus:");
+            Console.WriteLine("1 - Kütuse kalkulaator");
+            Console.WriteLine("2 - Isikukoodi analüüs");
+            Console.WriteLine("3 - Täringumäng");
+            Console.WriteLine("4 - Palgaarvestus");
+            Console.WriteLine("0 - Välju");
+            Console.Write("Sinu valik: ");
+
+            string valik = Console.ReadLine();
+            Console.WriteLine();
+
+            switch (valik)
+            {
+                case "1":
+                    Alamfunktsioonid.KytuseKalkulaator();
+                    break;
+
+                case "2":
+                    Console.Write("Sisesta isikukood: ");
+                    string ik = Console.ReadLine();
+                    string tulemus = Alamfunktsioonid.HindaIsikukood(ik);
+                    Console.WriteLine(tulemus + "\n");
+                    break;
+
+                case "3":
+                    Alamfunktsioonid.TaringuMang();
+                    break;
+
+                case "4":
+                    Console.Write("Sisesta brutopalk: ");
+                    double bruto = Convert.ToDouble(Console.ReadLine());
+                    var palk = Alamfunktsioonid.ArvutaPalk(bruto);
+
+                    Console.WriteLine($"Maksuvaba tulu: {palk.Item1:F2} €");
+                    Console.WriteLine($"Netopalk: {palk.Item2:F2} €\n");
+                    break;
+
+                case "0":
+                    tootab = false;
+                    Console.WriteLine("Programm lõpetatud.");
+                    break;
+
+                default:
+                    Console.WriteLine("Vigane valik!\n");
+                    break;
+            }
+        }
+    }
+}
