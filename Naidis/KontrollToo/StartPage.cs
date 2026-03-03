4f59554f9075e640d2f9bae04bca2a089c9287e2
shzyy <samsonov.eugene@bk.ru>
2026-03-03 22:48:00 +0200

    Update StartPage.cs

--- Naidis/KontrollToo/StartPage.cs
+++ Naidis/KontrollToo/StartPage.cs
@@ -1,59 +1,53 @@
 ﻿using System;
-
-class StartPage
-{
-    static void Main(string[] args)
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+        class StartPage
     {
-        bool tootab = true;
-
-        while (tootab)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Vali tegevus:");
-            Console.WriteLine("1 - Kütuse kalkulaator");
-            Console.WriteLine("2 - Isikukoodi analüüs");
-            Console.WriteLine("3 - Täringumäng");
-            Console.WriteLine("4 - Palgaarvestus");
-            Console.WriteLine("0 - Välju");
-            Console.Write("Sinu valik: ");
+            bool töötab = true;
 
-            string valik = Console.ReadLine();
-            Console.WriteLine();
-
-            switch (valik)
+            while (töötab)
             {
-                case "1":
-                    Alamfunktsioonid.KytuseKalkulaator();
-                    break;
+                Console.WriteLine("\nVali tegevus:");
+                Console.WriteLine("1 - Kütuse kalkulaator");
+                Console.WriteLine("2 - Isikukoodi analüüs");
+                Console.WriteLine("3 - Täringumäng");
+                Console.WriteLine("4 - Palgaarvestus");
+                Console.WriteLine("0 - Välju");
 
-                case "2":
-                    Console.Write("Sisesta isikukood: ");
-                    string ik = Console.ReadLine();
-                    string tulemus = Alamfunktsioonid.HindaIsikukood(ik);
-                    Console.WriteLine(tulemus + "\n");
-                    break;
+                Console.Write("Sisesta valik: ");
+                string valik = Console.ReadLine();
 
-                case "3":
-                    Alamfunktsioonid.TaringuMang();
-                    break;
+                switch (valik)
+                {
+                    case "1":
+                        Alamfunktsioonid.KytuseKalkulaator();
+                        break;
 
-                case "4":
-                    Console.Write("Sisesta brutopalk: ");
-                    double bruto = Convert.ToDouble(Console.ReadLine());
-                    var palk = Alamfunktsioonid.ArvutaPalk(bruto);
+                    case "2":
+                        Alamfunktsioonid.HindaIsikukood();
+                        break;
 
-                    Console.WriteLine($"Maksuvaba tulu: {palk.Item1:F2} €");
-                    Console.WriteLine($"Netopalk: {palk.Item2:F2} €\n");
-                    break;
+                    case "3":
+                        Alamfunktsioonid.TaringuMang();
+                        break;
 
-                case "0":
-                    tootab = false;
-                    Console.WriteLine("Programm lõpetatud.");
-                    break;
+                    case "4":
+                        Alamfunktsioonid.KysiJaArvutaPalk();
+                        break;
 
-                default:
-                    Console.WriteLine("Vigane valik!\n");
-                    break;
+                    case "0":
+                        töötab = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Vale valik!");
+                        break;
+                }
             }
         }
     }
-}
+
