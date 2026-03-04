1bd469c0cc234731f4aae2e8480db50af0c3998b
shzyy <samsonov.eugene@bk.ru>
2026-03-04 16:16:55 +0200

    Update menu options and functionality in StartPage

--- Naidis/KontrollToo/StartPage.cs
+++ Naidis/KontrollToo/StartPage.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-        class StartPage
+using System;
+
+namespace Jareltöö1
+{
+    class StartPage
     {
         static void Main(string[] args)
         {
@@ -12,31 +11,42 @@ using System.Threading.Tasks;
             while (töötab)
             {
                 Console.WriteLine("\nVali tegevus:");
-                Console.WriteLine("1 - Kütuse kalkulaator");
-                Console.WriteLine("2 - Isikukoodi analüüs");
-                Console.WriteLine("3 - Täringumäng");
-                Console.WriteLine("4 - Palgaarvestus");
+                Console.WriteLine("1 - Kinopiletite kalkulaator");
+                Console.WriteLine("2 - Kuupäeva eraldaja");
+                Console.WriteLine("3 - Mündimäng");
+                Console.WriteLine("4 - Ristküliku arvutused");
                 Console.WriteLine("0 - Välju");
-
                 Console.Write("Sisesta valik: ");
+
                 string valik = Console.ReadLine();
 
                 switch (valik)
                 {
                     case "1":
-                        Alamfunktsioonid.KytuseKalkulaator();
+                        Alamfunktsioonid.Piletikalkulaator();
                         break;
 
                     case "2":
-                        Alamfunktsioonid.HindaIsikukood();
+                        Console.Write("Sisesta kuupäev (YYYY-MM-DD): ");
+                        string kuup = Console.ReadLine();
+                        Alamfunktsioonid.HindaKuupaeva(kuup);
                         break;
 
                     case "3":
-                        Alamfunktsioonid.TaringuMang();
+                        Alamfunktsioonid.MundiMang();
                         break;
 
                     case "4":
-                        Alamfunktsioonid.KysiJaArvutaPalk();
+                        Console.Write("Sisesta pikkus: ");
+                        double pikkus = Convert.ToDouble(Console.ReadLine());
+
+                        Console.Write("Sisesta laius: ");
+                        double laius = Convert.ToDouble(Console.ReadLine());
+
+                        var tulemus = Alamfunktsioonid.ArvutaRistkylik(pikkus, laius);
+
+                        Console.WriteLine($"Pindala: {tulemus.Item1:F2}");
+                        Console.WriteLine($"Ümbermõõt: {tulemus.Item2:F2}");
                         break;
 
                     case "0":
@@ -50,4 +60,5 @@ using System.Threading.Tasks;
             }
         }
     }
+}
 
